ccd8047627e9b7dd7afa81d09481eabd4e14f477
Tim Rogers <tim@gocardless.com>
2014-02-01 22:07:35 +0000

    Don't automatically re-record cassettes, and thus break the specs with new data

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,10 +1,6 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.default_cassette_options = {
-    record: :new_episodes,
-    re_record_interval: 24 * 60 * 60
-  }
   c.cassette_library_dir = File.expand_path('../cassettes/', __FILE__)
   c.hook_into :webmock
   c.configure_rspec_metadata!
